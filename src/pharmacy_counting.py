31fbb5d4eab6b6de90ff50fedfa0cd713198e779
Thomas Ernste <tjernste@gmail.com>
2018-08-01 23:13:27 -0500

    updating Github repository

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -1,109 +1,63 @@
 import csv
-import time
-import datetime
 import sys
-import functools
+from collections import defaultdict
 
 
 def filter_pharm_data(input_data_txt, output_data_txt):
 
     #Open the file from the first argument (input_data_txt) and read it in as a list
-    with open(input_data_txt, 'r') as pharm_file:
-        reader = csv.reader(pharm_file)
+    with open(input_data_txt, 'r') as file:
+        reader = csv.reader(file)
         pharm_input = list(reader)
-
 
     # Preserve only the columns I need from the input data array to complete the data processing
     for row in pharm_input:
-        row.insert(row[3], row[4])
+        del row[:3]
+    pharm_input[0].insert(5, 'count_value')
+    for row in pharm_input[1:]:
+        row.insert(3, 1)
 
 
-    # #Convert the date_time column to a datetime object, then convert that to the time in UTC seconds
-    # for row in sec_input[1:]:
-    #     row[1] = datetime.datetime.strptime(row[1], '%Y-%m-%d_%H:%M:%S')
-    #     row[1] = str(int(time.mktime(row[1].timetuple())))
-    #
-    # # sec_input = [['user', 'time', 'address'], ['F', '0', 'a'], ['T', '0', 'b'], ['T', '0', 'c'], ['T', '1', 'd'],
-    # #         ['B', '1', 'e'], ['K', '2', 'f'], ['J', '2', 'g'], ['T', '3', 'h'], ['J', '4', 'i'], ['B', '4', 'j'],
-    # #         ['B', '5', 'k']]
-    #
-    #
-    # #initialize dictionary
-    # dct = {}
-    #
-    # #initalize list
-    # result = []
-    #
-    #
-    # for user, seconds in sec_input[1:]:
-    #     #if the user value for a row is equal to some 'ip' address, iterate through all of these ip addresses
-    #     if dct.get(user):
-    #         # If current time is within X (inactivity_value) seconds of last user's time....
-    #         if int(seconds) <= int(dct[user][-1]) + inactivity_value:
-    #             # The time between users is within X (inactivity_value) seconds of last user's time,
-    #             # so the code appends the time value to the dictionary for this user
-    #             dct[user].append(int(seconds))
-    #             # Skip the rest of this iteration
-    #             continue
-    #         # Skips here if there is not another previous request by this user within the time in the
-    #         # inactivity value table, so it creates a temporary list t with the data for this visit
-    #         t = dct[user]
-    #         # Appends the t list from above to the final result list
-    #         result.append([user, t[0], t[-1], (t[-1] - t[0] + 1), len(t)])
-    #
-    #     # Overwrites user in the dct with new data
-    #     dct[user] = [int(seconds)]
-    #
-    #     # Parses the rest of the dct here, appending each dictionary key and value to the list that have
-    #     # not already been appended to the list
-    #     # Also, the formula at index 3 in the list is the calculation that produces the duration column.
-    #     b = [[k, v[0], v[-1], (v[-1] - v[0] + 1), len(v)] for k, v in
-    #          dct.items()]
-    #
-    # # header = [["ip", "start_time", "end_time", "duration", "count"]]
-    #
-    # # Combines the previous result list that contains visits such as ip address where the user visited the
-    # # site and then made at least one more visit after the inactivity value expires. The code appends the early
-    # # visits by such a user before clearing the dictionary for the follow-up visit by the user after the
-    # # inactivity expiration time.
-    # result = sorted(result + b, key=lambda x: x[1:3])
-    #
-    # # Because I iterated through users rather than  by seconds, the rows in the 2d 'result' array at this
-    # # time are not sorted properly. The conditional_sort function sorts first in descending order by the
-    # # start_time. But in unique conditions, the rows are conditionally sorted in descending order by end_
-    # # time. Specifically, this function adds a condition that if -- for example -- we have two ip addresses,
-    # # and the first has an earlier start_time than the second, but the second has an end_time that occurs
-    # # before the expiration of the inactivity value for the first ip address, then the second ip address
-    # # will appear earlier in the output in such conditions.
-    #
-    # def conditional_sort(ip1, ip2):
-    #     start = 1
-    #     end = 2
-    #
-    #     if ip1[start] - ip2[start] >= inactivity_value:
-    #         return ip1[start] - ip2[start]
-    #
-    #     else:
-    #         return ip1[end] - ip2[end]
-    #
-    # result = [result[0]] + sorted(result[1:], key=functools.cmp_to_key(conditional_sort))
-    #
-    # # This loop converts the UTC seconds value for the date_time columns back to the datetime format.
-    # for row in result:
-    #     row[1] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(row[1]))
-    #     row[2] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(row[2]))
-    #
+    count_dict = {}  # Initialize final dict that will contain output data
+    cost_dict = {}
 
-    # This code writes the data one row at a time to the output .txt file
+    for drug, cost, counter in pharm_input[1:]:
+    # # #     #if the user value for a row is equal to some 'ip' address, iterate through all of these ip addresses
+
+        if drug in count_dict:
+            count_dict[drug] += 1
+        else:
+            count_dict[drug] = counter
+
+        if drug in cost_dict:
+            cost_dict[drug] += int(cost)
+        else:
+            cost_dict[drug] = int(cost)
+
+    final_dict = defaultdict(list)
+
+
+    for d in (count_dict, cost_dict):  # you can list as many input dicts as you want here
+        for key, value in d.items():
+            final_dict[key].append(value)
+
+    final_dict = dict(final_dict)
+
+    final_sorted_list = sorted(final_dict.items(), key=lambda kv: (-kv[1][1], kv[0]))
+
+
+    final_array = [[row[0], row[1][0], row[1][1]] for row in final_sorted_list]
+    final_array.insert(0, ['drug_name', 'num_prescriber', 'total_cost'])
+
     with open(output_data_txt, 'w') as myfile:
         wr = csv.writer(myfile)
-        for i in result:
+        for i in final_array:
             wr.writerow(i)
 
 def main():
-    input_data_csv = sys.argv[1]
-    output_data_csv = sys.argv[2]
-    filter_pharm_data(input_data_csv, output_data_csv)
+    input_data_txt = sys.argv[1]
+    output_data_txt = sys.argv[2]
+    filter_pharm_data(input_data_txt, output_data_txt)
 
 if __name__ == '__main__':
 	main()
